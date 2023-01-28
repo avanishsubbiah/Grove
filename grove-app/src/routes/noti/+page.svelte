ece8d23afeaa8281a6cc0176145af3d809d25ab7
avanishsubbiah <avanishsubbiah1@gmail.com>
2023-01-28 13:24:52 -0500

    Added basic page nav

---
+++ grove-app/src/routes/noti/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline">
+    Welcome to your Notifications!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.green.100);
+    }
+</style>
