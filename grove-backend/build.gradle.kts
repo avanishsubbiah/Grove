6629fb0d384cca163e1c1d40e6128c3937c1af08
Jackson <jrmoffet@gmail.com>
2023-01-28 19:22:41 -0500

    Nuke from orbit

--- grove-backend/build.gradle.kts
+++ grove-backend/build.gradle.kts
@@ -15,39 +15,37 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     kapt("io.micronaut.data:micronaut-data-processor")
     kapt("io.micronaut:micronaut-http-validation")
     kapt("io.micronaut.security:micronaut-security-annotations")
-    kapt("io.micronaut.serde:micronaut-serde-processor")
     implementation("io.micronaut:micronaut-http-client")
-    implementation("io.micronaut.data:micronaut-data-r2dbc")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
     implementation("io.micronaut.flyway:micronaut-flyway")
+    implementation("io.micronaut.graphql:micronaut-graphql")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.reactor:micronaut-reactor")
     implementation("io.micronaut.reactor:micronaut-reactor-http-client")
-    implementation("io.micronaut.security:micronaut-security-session")
-    implementation("io.micronaut.views:micronaut-views-velocity")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut.security:micronaut-security-jwt")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-    implementation("io.micronaut.flyway:micronaut-flyway")
     implementation("jakarta.annotation:jakarta.annotation-api")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     runtimeOnly("ch.qos.logback:logback-classic")
-    runtimeOnly("dev.miku:r2dbc-mysql")
-    runtimeOnly("mysql:mysql-connector-java")
-    runtimeOnly("org.flywaydb:flyway-mysql")
+    runtimeOnly("org.postgresql:postgresql")
+    compileOnly("org.graalvm.nativeimage:svm")
+
     implementation("io.micronaut:micronaut-validation")
 
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-    runtimeOnly("org.flywaydb:flyway-mysql")
-    compileOnly("org.graalvm.nativeimage:svm")
+
 }
 
 
 application {
-    mainClass.set("io.grove.ApplicationKt")
+    mainClass.set("example.micronaut.ApplicationKt")
 }
 java {
     sourceCompatibility = JavaVersion.toVersion("11")
@@ -71,7 +69,7 @@ micronaut {
     testRuntime("junit5")
     processing {
         incremental(true)
-        annotations("io.grove.*")
+        annotations("example.micronaut.*")
     }
 }
 
--- grove-backend/src/main/kotlin/io/grove/domain/User.kt
+++ grove-backend/src/main/kotlin/io/grove/domain/User.kt
@@ -4,13 +4,9 @@ import io.micronaut.serde.annotation.Serdeable
 import io.micronaut.data.annotation.GeneratedValue
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
+import javax.validation.constraints.NotBlank
 
-
-@Serdeable
 @MappedEntity
-data class User(
-    @field: Id
-    @field: GeneratedValue(GeneratedValue.Type.AUTO)
-    var id: Long? = null,
-    var name: String
-)
+class Users(var name: @NotBlank String?, var username: @NotBlank String?) {
+    
+}
