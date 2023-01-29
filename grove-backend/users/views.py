0eb2e1a13714f6d5b8d96a09bcd1bdacdaa3c757
Jackson <jrmoffet@gmail.com>
2023-01-29 10:29:31 -0500

    Send user id

--- grove-backend/users/views.py
+++ grove-backend/users/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpRequest, Http404
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
 from groves.models import Grove
-from notifications.models import Notification
 from .serializers import UserSerializer
-from django.db.models import Q
 
 
 @api_view(["GET"])
@@ -19,4 +15,9 @@ def list_users(request):
 
 @api_view(["GET"])
 def list_users_groves(request):
-    return Response(UserSerializer(Grove.get_friends(User.get_by_username(request.user)), many=True).data)
+    return Response(
+        UserSerializer(
+            Grove.get_friends(User.get_by_username(request.user)),
+            many=True
+        ).data
+    )
