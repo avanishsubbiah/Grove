e43fe34cc70de1fce910f23217cde076cfaa6fd9
Jackson <jrmoffet@gmail.com>
2023-01-28 21:07:54 -0500

    Update models

--- grove-backend/groves/models.py
+++ grove-backend/groves/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+class Grove(models.Model):
+    user_a = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_b = models.ForeignKey(User, on_delete=models.CASCADE)
+    xp = models.IntegerField()
+    start_date = models.DateField()
--- grove-backend/users/models.py
+++ grove-backend/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import QuerySet
+from grooves.model import Grove
+
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def get_groves(self) -> QuerySet:
+        return self.grove_set.all()
+
+    def get_grove(self, other: User) -> Grove:
