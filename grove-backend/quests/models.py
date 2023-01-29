f997a9abc817a39aa077e05071dbc4fe01a1ccbe
Connor Chadwick <cchad2019@gmail.com>
2023-01-28 21:15:52 -0500

    questing for friends

--- grove-backend/quests/models.py
+++ grove-backend/quests/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+class Quest(models.Model):
+    decription = models.CharField(max_length=1024)
+    status = models.BooleanField()
+    user_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_from = models.ForeignKey(User, on_delete=models.CASCADE)
+    exp = models.IntegerField()
